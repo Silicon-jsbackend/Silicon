0a6ee0ab037ec4118394a8e58c2cbfb875cf4ced
Edvin <edvin.thornholm@gmail.com>
2024-05-20 09:54:42 +0200

    Courses works

---
+++ BlazorApp/BlazorApp/wwwroot/site.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var menuButton = document.getElementById('mobil-menu-btn');
+    var mobileMenu = document.getElementById('mobil-menu');
+
+    if (menuButton && mobileMenu) {
+        menuButton.addEventListener('click', function () {
+            console.log('Knappen klickades');
+            mobileMenu.classList.toggle('open');
+            var ariaExpanded = mobileMenu.getAttribute('aria-expanded');
+            mobileMenu.setAttribute('aria-expanded', ariaExpanded === 'true' ? 'false' : 'true');
+        });
+    } else {
+        console.error('Knappen eller mobilmenyn kunde inte hittas.');
+    }
+});
